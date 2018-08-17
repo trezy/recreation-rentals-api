1e24228f548b86ce8f6ce75ece2053ad638022f3
Trezy <tre@trezy.com>
2018-08-17 09:47:30 -0500

    Abstract route builder
    Also, make it recursive so it can go deeper into the directory
    structure.

---
+++ server/helpers/buildRoutesFromDirectoryStructure.js
@@ -0,0 +1,40 @@
+// Module imports
+const { resolve } = require('path')
+const {
+  lstatSync,
+  readdirSync,
+} = require('fs')
+const Router = require('koa-router')
+
+
+
+
+
+// Component constants
+const baseRoutesPath = resolve(__dirname, '..', 'routes')
+
+
+
+
+
+module.exports = function buildRoutesFromDirectoryStructure (router, currentPath = '/') {
+  const routesPath = currentPath ? (baseRoutesPath + currentPath) : baseRoutesPath
+  const routesDirContents = readdirSync(routesPath)
+
+  for (const routeFilename of routesDirContents) {
+    const routePath = resolve(routesPath, routeFilename)
+
+    if (lstatSync(routePath).isDirectory()) {
+      const entityType = routeFilename
+      const subrouter = new Router
+
+      buildRoutesFromDirectoryStructure(subrouter, currentPath + routeFilename)
+
+      return router.use(`/${entityType}`, subrouter.routes(), subrouter.allowedMethods())
+    }
+
+    const route = new (require(routePath))
+
+    router[route.method.toLowerCase()](route.url, route._handleRequest)
+  }
+}
--- server/router.js
+++ server/router.js
@@ -12,6 +12,13 @@ const Router = require('koa-router')
 
 
 
+// Module imports
+const buildRoutesFromDirectoryStructure = require('./helpers/buildRoutesFromDirectoryStructure')
+
+
+
+
+
 // Component constants
 const router = new Router
 
@@ -20,45 +27,7 @@ const router = new Router
 
 
 module.exports = koa => {
-  const routesDirPath = resolve(__dirname, 'routes')
-  const routesDirContents = readdirSync(routesDirPath)
-
-  /*
-   * This section reads all of the items from the `routes` directory and
-   * implements them appropriately. If the item is a file, it gets the route
-   * info from the file and injects the route. If it's a directory, it gets the
-   * index file and mounts it as a subrouter.
-   */
-  for (const routeFilename of routesDirContents) {
-    const routePath = resolve(routesDirPath, routeFilename)
-
-    if (extname(routeFilename) === '.js') {
-      const route = new (require(routePath))
-
-      router[route.method.toLowerCase()](route.url, route._handleRequest)
-    } else {
-      const entityType = routeFilename
-      const subroutesDirContents = readdirSync(routePath)
-      const subrouter = new Router
-
-      for (const subrouteFilename of subroutesDirContents) {
-        const subroutePath = resolve(routePath, subrouteFilename)
-        const subroute = new (require(subroutePath))
-
-        subrouter[subroute.method.toLowerCase()](subroute.url, subroute._handleRequest)
-      }
-
-      router.use(`/${entityType}`, subrouter.routes(), subrouter.allowedMethods())
-    }
-  }
-
-
-
-
-
-  /******************************************************************************\
-    Attach the router to the app
-  \******************************************************************************/
+  buildRoutesFromDirectoryStructure(router)
 
   koa.use(router.routes())
   koa.use(router.allowedMethods())
