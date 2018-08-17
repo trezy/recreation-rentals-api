70cdf8720c981d040779fb7d6c145b5cda61bb88
Trezy <tre@trezy.com>
2018-08-17 11:54:48 -0500

    Update env variable names

--- server/config.js
+++ server/config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  braintree: {
-    merchantAccountId: process.env.BTAPI_BRAINTREE_MERCHANT_ACCOUNT_ID,
-    merchantId: process.env.BTAPI_BRAINTREE_MERCHANT_ID,
-    publicKey: process.env.BTAPI_BRAINTREE_PUBLIC_KEY,
-    privateKey: process.env.BTAPI_BRAINTREE_PRIVATE_KEY,
-  },
   stripe: {
-    key: process.env.BTAPI_STRIPE_KEY,
-    secret: process.env.BTAPI_STRIPE_SECRET,
+    secret: process.env.PAYAPI_STRIPE_SECRET,
   }
 }
