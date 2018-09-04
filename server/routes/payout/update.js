95ce4c8b3520b909f80a57d4390d4be307b6c1a7
Trezy <tre@trezy.com>
2018-09-03 22:41:23 -0500

    Add a route for receiving charge update webhooks

---
+++ server/routes/payout/update.js
@@ -0,0 +1,64 @@
+// Module imports
+const PropTypes = require('prop-types')
+
+
+
+
+
+// Component imports
+const BaseRoute = require('../../helpers/BaseRoute')
+
+
+
+
+
+class UpdatePayoutEndpoint extends BaseRoute {
+  /***************************************************************************\
+    Public methods
+  \***************************************************************************/
+
+  async handleRequest (ctx, params) {
+    console.log(ctx.body)
+    // const { stripe } = ctx
+    // const {
+    //   amount,
+    //   destinationAccountID,
+    //   destinationBankAccountID,
+    // } = params
+
+    // await stripe.transfers.create({
+    //   amount,
+    //   currency: 'usd',
+    //   destination: destinationAccountID,
+    // })
+
+    // stripe.payouts.create({
+    //   amount,
+    //   currency: 'usd',
+    //   destination: destinationBankAccountID,
+    // }, {
+    //   stripe_account: destinationAccountID,
+    // })
+  }
+
+
+
+
+  /***************************************************************************\
+    Getters
+  \***************************************************************************/
+
+  get method () {
+    return 'post'
+  }
+
+  get url () {
+    return '/update'
+  }
+}
+
+
+
+
+
+module.exports = UpdatePayoutEndpoint
